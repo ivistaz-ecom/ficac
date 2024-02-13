770bfdda0c5c075dccbb9c26e5f665f18c0e8915
jalal1985khan <jalal1985khan@gmail.com>
2024-02-13 14:47:45 +0530

    removed image host name form the next config file

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['beta.ficacworld.org','walmartvriddhi.vercel.app','ficacworld.org'],
+        domains: ['ficacworld.org'],
       },
-
 }
 
 module.exports = nextConfig
